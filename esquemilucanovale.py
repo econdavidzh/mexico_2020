50e637c05698f3b17047385abcb53d7d7755d900
David Zambrano <82787004+econdavidzh@users.noreply.github.com>
2022-01-18 10:47:00 -0500

    Update esquemilucanovale.py

--- esquemilucanovale.py
+++ esquemilucanovale.py
@@ -112,6 +112,8 @@ st.write(pd.DataFrame({
 }).round(2), use_column_width = True)
 st.write("Última actualización:", fecha_actualizacion)
 
+st.image(luca, use_column_width = False, width = 150)
+
 # Cambiar de USD a MEX:
 la_reluca = st.number_input('Cuánta luca (en dólares) quieres cambiar a pesos mexicanos?')
 la_reluca = float(la_reluca)
@@ -133,9 +135,6 @@ if precio_en_mex != 0:
     st.image(mal, width = 180)
     st.write("Esto mismo en dolarucos te cuesta $",
             round(precio_en_mex / pesos_mexicanos_por_1_dolar, 2))
-
-if precio_en_mex != 0:
-    st.image(luca, use_column_width = False, width = 150)
 
 # Dividir una cuenta:
 cuenta_dividida = st.number_input('Quieres dividir una cuenta?')
